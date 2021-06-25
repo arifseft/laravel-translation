136ae5e8f1d98e64c10f32aaa45213f9183d7752
arif-transflynd <arif@transflynd.com>
2021-06-25 18:08:19 +0700

    fix request rules

--- src/Http/Requests/TranslationRequest.php
+++ src/Http/Requests/TranslationRequest.php
@@ -6,26 +6,27 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TranslationRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'key' => 'required',
-            'value' => 'required',
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'key' => 'required',
+			// 'value' => 'required',
+			'values' => 'required|array',
+		];
+	}
 }
